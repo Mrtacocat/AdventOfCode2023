1214bf344c203018030492d8bd595cbfcce08f6e
mrtacocat <Christoffer@riis.cc>
2024-05-20 00:26:12 +0200

    day 1 complete

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,29 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
         try {
-            File dataset = new File("src/Dataset/testset");
-            Scanner scanner = new Scanner(dataset);
-
-            int sum = 0;
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                int firstDigit = -1;
-                int lastDigit = -1;
-
-                for (int i = 0; i < line.length(); i++) {
-                    char c = line.charAt(i);
-                    if (Character.isDigit(c)) {
-                        int number = Integer.parseInt(String.valueOf(c));
-                        if(firstDigit == -1) {
-                            firstDigit = number;
+            BufferedReader br = new BufferedReader(new FileReader("src/Dataset/dataset"));
+            int val = 0;
+            for (String line : br.lines().toList()) {
+                int first = -1;
+                int last = -1;
+                for (String c : line.split("")) {
+                    if (Character.isDigit(c.charAt(0))) {
+                        if (first == -1) {
+                            first = Integer.parseInt(c);
                         }
-                        lastDigit = number;
+                        last = Integer.parseInt(c);
                     }
                 }
-
-                if (firstDigit != -1) {
-                    System.out.println("First digit in \"" + line + "\": " + firstDigit);
-                } else {
-                    System.out.println("No digit found in \"" + line + "\".");
-                }
-
-                if (lastDigit != -1) {
-                    System.out.println("Last digit in \"" + line + "\": " + lastDigit);
-                } else {
-                    System.out.println("No digit found in \"" + line + "\".");
-                }
-
-                int calibration = firstDigit + lastDigit;
-                sum += calibration;
+                first *= 10;
+                val += first + last;
             }
-            System.out.println(sum);
-        } catch (Exception e) {
-            System.out.println("ERROR: " + e.toString());
+            System.out.println(val);
+        } catch (FileNotFoundException ex) {
+            throw new RuntimeException(ex);
         }
-
-
     }
 }
